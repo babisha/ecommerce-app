f6155b09fbf84618d17a4b01e5fd1c31e0f2fb36
Babisha Manandhar <mdr.bebo@gmail.com>
2023-07-25 09:11:09 +0400

    Homepage added

---
+++ src/Pages/Homepage.tsx
@@ -0,0 +1,44 @@
+// import React, { ChangeEvent } from 'react'
+
+// function Homepage() {
+//     interface IObj {
+//         name: string | null,
+//         number: Number | null,
+//         newObj : INewobj,
+//     };
+//     interface INewobj {
+//         sub : string | null,
+//         major : string | null,
+//     }
+//     const data:string = "babisha"
+//     const obj: IObj = {
+//         name: "Babisha",
+//         number: 20,
+//         newObj: {
+//             sub: "Science",
+//             major: "Med",
+//         }
+//     };
+
+//     const handleChange = (event:ChangeEvent<HTMLInputElement>) => {
+//         const value = event.target.value
+
+//     };
+//    const add = () : string => {
+//     return "babisha";
+        
+//     }
+//     function substract() : number {
+//         return 2;
+//     }
+    
+//   return (
+//     <div>
+//         <input type='text' onChange={handleChange}></input>
+//     </div>
+//   )
+// }
+
+
+
+// export default Homepage
