3779dd7c996f440a044abb0f2a539b2ee360d2fc
JunuThapa <junuthapa011@gmail.com>
2023-07-26 19:21:31 -0400

    added register page, and css

---
+++ src/Pages/Register.page.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div className="form">
+      <form>
+        <h2>Register Here </h2>
+        <label htmlFor="FullName">Full Name: </label>
+        <input
+          type="text"
+          id="FullName"
+          placeholder="Enter your full Name..."
+        />
+        <label htmlFor="UserName">Username: </label>
+        <input type="text" id="UserName" placeholder="Enter your username..." />
+        <label htmlFor="EmailAddress">Email address: </label>
+        <input
+          type="text"
+          id="EmailAddress"
+          placeholder="Enter your email address..."
+        />
+        <label htmlFor="Password">Password: </label>
+        <input
+          type="password"
+          id="Password"
+          placeholder="Enter your password..."
+        />
+        <label htmlFor="ConfirmPassword">Confirm Password: </label>
+        <input
+          type="password"
+          id="ConfirmPassword"
+          placeholder="Enter your password again..."
+        />
+        <button>SIGN UP</button>
+        <p>
+          Already have an account? <span>Sign in.</span>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
