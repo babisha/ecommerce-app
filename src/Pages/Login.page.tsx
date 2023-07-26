0332161fe3196cf0ac8c1ad5214940dbcc12d536
JunuThapa <junuthapa011@gmail.com>
2023-07-26 19:59:26 -0400

    feat: loginpage, routes, react-router-dom

---
+++ src/Pages/Login.page.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="form">
+      <form>
+        <h2>Login Here </h2>
+        <label htmlFor="UserName">Username: </label>
+        <input type="text" id="UserName" placeholder="Enter your username..." />
+
+        <label htmlFor="Password">Password: </label>
+        <input
+          type="password"
+          id="Password"
+          placeholder="Enter your password..."
+        />
+
+        <button>SIGN IN</button>
+
+        <p>Forgot your password?</p>
+        <p>
+          New user? <span>Sign Up.</span>
+        </p>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
