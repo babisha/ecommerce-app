126688e64d633e7c50f374773d9d4c112b3d9dcb
Babisha Manandhar <mdr.bebo@gmail.com>
2023-07-30 01:52:58 +0400

    new branch added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import Register from "./Pages/Register.page";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./Pages/Login.page";
+import Homepage from "./Pages/Home.page";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Register />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/homepage" element={<Homepage />} />
     </Routes>
   );
 };
---
+++ src/Pages/Home.page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div className='text-3xl font-bold underline'>
+        HelloWorld
+    </div>
+  )
+}
+
+export default Homepage
